d131bed86e7ef181340ab7c6b1724e3f25e2f410
ananasdenisovich <113703176+ananasdenisovich@users.noreply.github.com>
2023-11-06 00:20:56 +0600

    Update getSTartedScript.js

--- getSTartedScript.js
+++ getSTartedScript.js
@@ -1,8 +1,8 @@
-//Timer
-document.addEventListener('DOMContentLoaded', function() { //countdown converting from previous labs
+//timer
+document.addEventListener('DOMContentLoaded', function() {
   const timerDisplay = document.getElementById('timer');
   const progressBar = document.querySelector('.progress-bar');
-  let countdown;
+  let animationFrameId;
 
   function updateCountdown() {
     const now = new Date();
@@ -15,14 +15,33 @@ document.addEventListener('DOMContentLoaded', function() { //countdown convertin
     const seconds = Math.floor((timeRemaining % (1000 * 60)) / 1000);
 
     const display = `${String(days).padStart(2, '0')}:${String(hours).padStart(2, '0')}:${String(minutes).padStart(2, '0')}:${String(seconds).padStart(2, '0')}`;
-    timerDisplay.textContent = display;//outputs a timer
+    timerDisplay.textContent = display;
 
     const percentage = (timeRemaining / (365 * 24 * 60 * 60 * 1000)) * 100;
-    progressBar.style.width = 100 - percentage + '%';
+
+    progressBar.style.width = '0%'; 
+
+    const start = performance.now();
+
+    function animateProgress(currentTime) {
+      const elapsed = currentTime - start;
+      const progress = Math.min(elapsed / 5000, 1);
+
+      progressBar.style.width = `${progress * (100 - percentage)}%`;
+
+      if (progress < 1) {
+        animationFrameId = requestAnimationFrame(animateProgress);
+      }
+    }
+
+    animateProgress(performance.now());
   }
 
   updateCountdown();
-  countdown = setInterval(updateCountdown, 1000);//sets an interval for second changing
+  countdown = setInterval(updateCountdown, 1000);
+  document.addEventListener('visibilitychange', function() {
+    if (document.hidden) {
+      cancelAnimationFrame(animationFrameId);
+    }
+  });
 });
-
-
